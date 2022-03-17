fea45a44a6ae606d846ed2f7501e966789fc94ef
Vaclav Tarantik <vaclavtarantik@vaclavs-mbp.lan>
2022-03-17 10:48:49 +0100

    Implement starter code including dummy data for week 2

---
+++ app/src/main/java/com/strv/movies/model/Trailer.kt
@@ -0,0 +1,14 @@
+package com.strv.movies.model
+
+data class Trailer(
+    val iso_639_1: String,
+    val iso_3166_1: String,
+    val name: String,
+    val key: String,
+    val site: String,
+    val size: Int,
+    val type: String,
+    val official: Boolean,
+    val publishedAt: String,
+    val id: String
+)
