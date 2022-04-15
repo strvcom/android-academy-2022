9b491c8d0294d8a0ac1f351ede3a4e63de19abb0
JurajKuliska <juraj.kuliska@strv.com>
2022-04-15 17:22:41 +0200

    Wrap posters in Card and adjust paddings

--- app/src/main/java/com/strv/movies/MainActivity.kt
+++ app/src/main/java/com/strv/movies/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.strv.movies.data.OfflineMoviesProvider
-import com.strv.movies.ui.moviedetail.MovieDetail
+import com.strv.movies.ui.movieslist.MoviesList
 import com.strv.movies.ui.theme.MoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +46,7 @@ class MainActivity : ComponentActivity() {
                                 DarkLightModeSwitchIcon(isDarkTheme = isDarkTheme)
                             }
                         )
-                        MovieDetail(movie = OfflineMoviesProvider.getMovieDetail(1))
+                        MoviesList(movies = OfflineMoviesProvider.getMovies())
                     }
                 }
             }
--- app/src/main/java/com/strv/movies/ui/movieslist/MoviesListScreen.kt
+++ app/src/main/java/com/strv/movies/ui/movieslist/MoviesListScreen.kt
@@ -1,27 +1,36 @@
 package com.strv.movies.ui.movieslist
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.strv.movies.R
 import com.strv.movies.model.Movie
 
 @Composable
 fun MovieItem(movie: Movie) {
-    AsyncImage(
-        model = "https://image.tmdb.org/t/p/w500${movie.posterPath}",
-        contentDescription = stringResource(id = R.string.movie_image)
-    )
+    Card(modifier = Modifier.padding(all = 8.dp)) {
+        AsyncImage(
+            contentScale = ContentScale.FillBounds,
+            model = "https://image.tmdb.org/t/p/h632${movie.posterPath}",
+            contentDescription = stringResource(id = R.string.movie_image)
+        )
+    }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MoviesList(movies: List<Movie>) {
     LazyVerticalGrid(
+        modifier = Modifier.padding(8.dp),
         cells = GridCells.Fixed(2)
     ) {
         items(movies) { movie ->
