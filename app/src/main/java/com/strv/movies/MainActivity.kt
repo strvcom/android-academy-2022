3f3328889104d33b6eb9e519ad4cb9e558550093
JurajKuliska <juraj.kuliska@strv.com>
2022-04-15 16:29:15 +0200

    Add top appbar

--- app/src/main/java/com/strv/movies/MainActivity.kt
+++ app/src/main/java/com/strv/movies/MainActivity.kt
@@ -3,12 +3,15 @@ package com.strv.movies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.strv.movies.data.OfflineMoviesProvider
 import com.strv.movies.ui.moviedetail.MovieDetail
@@ -24,7 +27,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MovieDetail(movie = OfflineMoviesProvider.getMovieDetail(1))
+                    Column {
+                        TopAppBar(
+                            title = {
+                                Text(text = stringResource(id = R.string.app_name))
+                            },
+                            backgroundColor = MaterialTheme.colors.primary
+                        )
+                        MovieDetail(movie = OfflineMoviesProvider.getMovieDetail(1))
+                    }
                 }
             }
         }
