2ec6afe3ae19a7788d517c798ea14f874dcda6b9
JurajKuliska <juraj.kuliska@strv.com>
2022-04-15 17:48:08 +0200

    Add an enter animation for the list items

--- app/src/main/java/com/strv/movies/ui/movieslist/MoviesListScreen.kt
+++ app/src/main/java/com/strv/movies/ui/movieslist/MoviesListScreen.kt
@@ -1,5 +1,11 @@
 package com.strv.movies.ui.movieslist
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
@@ -7,6 +13,7 @@ import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
@@ -16,8 +23,10 @@ import com.strv.movies.R
 import com.strv.movies.model.Movie
 
 @Composable
-fun MovieItem(movie: Movie) {
-    Card(modifier = Modifier.padding(all = 8.dp)) {
+fun MovieItem(movie: Movie, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(all = 8.dp)
+    ) {
         AsyncImage(
             contentScale = ContentScale.FillBounds,
             model = "https://image.tmdb.org/t/p/h632${movie.posterPath}",
@@ -26,7 +35,7 @@ fun MovieItem(movie: Movie) {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun MoviesList(movies: List<Movie>) {
     LazyVerticalGrid(
@@ -34,7 +43,21 @@ fun MoviesList(movies: List<Movie>) {
         cells = GridCells.Fixed(2)
     ) {
         items(movies) { movie ->
-            MovieItem(movie)
+            val state = remember {
+                MutableTransitionState(false).apply {
+                    // Start the animation immediately.
+                    targetState = true
+                }
+            }
+            AnimatedVisibility(
+                visibleState = state,
+                enter = fadeIn(animationSpec = tween(300)) + scaleIn(animationSpec = tween(300))
+            ) {
+                MovieItem(
+                    movie = movie,
+                    modifier = Modifier.animateItemPlacement()
+                )
+            }
         }
     }
 }
