002c7348d99a67ede0da454a3eb1e14c02a18cfa
Lukáš Valkovič <lukasvalkovic@users.noreply.github.com>
2022-04-30 00:46:35 +0200

    Test different states on movie detail

--- app/src/main/java/com/strv/movies/ui/moviedetail/MovieDetailViewModel.kt
+++ app/src/main/java/com/strv/movies/ui/moviedetail/MovieDetailViewModel.kt
@@ -2,12 +2,17 @@ package com.strv.movies.ui.moviedetail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.strv.movies.data.OfflineMoviesProvider
 import com.strv.movies.ui.navigation.MoviesNavArguments
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class MovieDetailViewModel @Inject constructor(
@@ -21,4 +26,20 @@ class MovieDetailViewModel @Inject constructor(
 
     private val _viewState = MutableStateFlow(MovieDetailViewState(loading = true))
     val viewState = _viewState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _viewState.update {
+                val randomNumber = Random.nextInt(10)
+                if (randomNumber < 3) {
+                    MovieDetailViewState(error = "Something went wrong!")
+                } else {
+                    MovieDetailViewState(
+                        movie = OfflineMoviesProvider.getMovieDetail(movieId)
+                    )
+                }
+            }
+        }
+    }
 }
