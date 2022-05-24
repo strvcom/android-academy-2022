ae5547315d8b2e8df59caf5a57748a0526587d42
Jakub Gongol <gongol.jakub@gmail.com>
2022-05-24 15:37:35 +0200

    Transaction usage

--- app/src/main/java/com/strv/movies/network/MovieRepository.kt
+++ app/src/main/java/com/strv/movies/network/MovieRepository.kt
@@ -1,13 +1,16 @@
 package com.strv.movies.network
 
+import androidx.room.withTransaction
 import com.strv.movies.data.dao.MoviesDao
 import com.strv.movies.data.mapper.MovieDetailMapper
 import com.strv.movies.data.mapper.MovieMapper
 import com.strv.movies.data.mapper.toDomain
 import com.strv.movies.data.mapper.toEntity
+import com.strv.movies.database.MoviesDatabase
 import com.strv.movies.extension.Either
 import com.strv.movies.model.Movie
 import com.strv.movies.model.MovieDetail
+import com.strv.movies.model.MovieDetailDTO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -18,17 +21,14 @@ class MovieRepository @Inject constructor(
     private val api: MovieApi,
     private val moviesDao: MoviesDao,
     private val movieDetailMapper: MovieDetailMapper,
-    private val movieMapper: MovieMapper
+    private val movieMapper: MovieMapper,
+    private val moviesDatabase: MoviesDatabase
 ) {
 
     suspend fun getMovieDetail(movieId: Int): Either<String, MovieDetail> {
         return try {
             val movie = api.getMovieDetail(movieId)
-
-            moviesDao.insertMovieDetail(movie.toEntity())
-            moviesDao.insertGenres(movie.genres.map { it.toEntity() })
-            moviesDao.insertMovieGenres(movie.genres.map { it.toEntity(movie.id) })
-
+            storeMovieDetail(movie)
             Either.Value(movieDetailMapper.map(movie))
         } catch (exception: Throwable) {
             Either.Error(exception.localizedMessage ?: "Network error")
@@ -48,4 +48,12 @@ class MovieRepository @Inject constructor(
         moviesDao.observeMovieDetail(movieId).map {
             it?.toDomain()
         }
+
+    private suspend fun storeMovieDetail(movie: MovieDetailDTO) {
+        moviesDatabase.withTransaction {
+            moviesDao.insertMovieDetail(movie.toEntity())
+            moviesDao.insertGenres(movie.genres.map { it.toEntity() })
+            moviesDao.insertMovieGenres(movie.genres.map { it.toEntity(movie.id) })
+        }
+    }
 }
