533be0339ebd58f686f4d63a87d684f4f01a5587
Jiri Parizek <jiri.parizek@strv.com>
2022-05-30 16:30:35 +0200

    Open AvatarEditDialog

--- app/src/main/java/com/strv/movies/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/strv/movies/ui/profile/ProfileScreen.kt
@@ -7,9 +7,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.strv.movies.ui.profile.components.AvatarEditDialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.strv.movies.ui.profile.components.ProfileAvatar
 
@@ -25,10 +30,20 @@ fun ProfileScreen(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var openDialog by remember { mutableStateOf(false) }
+
+        if(openDialog){
+            AvatarEditDialog(
+                onDismiss = { openDialog = false },
+                editAvatar = {},
+                removeAvatar = {}
+            )
+        }
+
         ProfileAvatar(
             url = "https://www.looper.com/img/gallery/20-epic-movies-like-avatar-you-need-to-watch-next/l-intro-1645555067.jpg",
             onEditClick = {
-                // TODO
+                openDialog = true
             }
         )
         Button(
