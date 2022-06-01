7cfe025415545d94a015600aaa0acd6ce5080676
Jiri Parizek <jiri.parizek@strv.com>
2022-06-01 09:04:41 +0200

    Refactor auth preferences

--- app/src/main/java/com/strv/movies/database/AuthDataStore.kt
+++ app/src/main/java/com/strv/movies/database/AuthDataStore.kt
@@ -16,7 +16,7 @@ import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Singleton
 
-private const val SESSION_TOKEN = "accessToken"
+private val SESSION_TOKEN = stringPreferencesKey("accessToken")
 private const val AUTH_DATASTORE = "auth_datastore"
 
 
@@ -24,45 +24,28 @@ private const val AUTH_DATASTORE = "auth_datastore"
 class AuthDataStore @Inject constructor(
     @ApplicationContext private val context: Context
 ) {
-    companion object{
-        val SESSION_TOKEN_KEY = stringPreferencesKey(SESSION_TOKEN)
-    }
-
     private val dataStore = PreferenceDataStoreFactory.create {
         context.preferencesDataStoreFile(AUTH_DATASTORE)
     }
 
-    val sessionToken: String? get() = dataStore.getOrNull(SESSION_TOKEN_KEY)
-
+    val sessionToken: String? get() = runBlocking {
+        dataStore.data
+            .map { preferences ->
+                preferences[SESSION_TOKEN]
+            }.first()
+    }
     val isAuthenticated: Boolean get() = sessionToken != null
-    val isAuthenticatedFlow: Flow<Boolean> get() = dataStore.getOrNullFlow(SESSION_TOKEN_KEY).map { it != null }
+    val isAuthenticatedFlow: Flow<Boolean> get() =  dataStore.data
+        .map { preferences ->
+            preferences[SESSION_TOKEN] != null
+        }
 
     suspend fun updateSessionToken(token: String) {
-        dataStore.updateValue(SESSION_TOKEN_KEY, token)
+        dataStore.edit { it[SESSION_TOKEN] = token }
     }
 
     suspend fun deleteSessionToken(){
-        dataStore.edit { it.remove(SESSION_TOKEN_KEY) }
-    }
-
-}
-
-fun <T> DataStore<Preferences>.getOrNull(key: Preferences.Key<T>): T? {
-    return runBlocking { getOrNullFlow(key).first() }
-}
-
-fun <T> DataStore<Preferences>.getOrNullFlow(key: Preferences.Key<T>): Flow<T?> {
-    return data.map { preferences ->
-        preferences[key]
+        dataStore.edit { it.remove(SESSION_TOKEN) }
     }
 }
 
-suspend inline fun <T> DataStore<Preferences>.updateValue(key: Preferences.Key<T>, value: T?) {
-    edit {
-        it.update(key, value)
-    }
-}
-
-fun <T> MutablePreferences.update(key: Preferences.Key<T>, value: T?) {
-    value?.let { this[key] = it } ?: remove(key)
-}
