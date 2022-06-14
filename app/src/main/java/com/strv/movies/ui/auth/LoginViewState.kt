c005e4b7b9a6ae1a451bb82782bd7c81294c9560
resonus <braun.hynek@gmail.com>
2022-06-14 20:35:10 +0200

    Deleted redundant error in login state

--- app/src/main/java/com/strv/movies/ui/auth/LoginViewState.kt
+++ app/src/main/java/com/strv/movies/ui/auth/LoginViewState.kt
@@ -2,6 +2,5 @@ package com.strv.movies.ui.auth
 
 data class LoginViewState(
     val user: String = "",
-    val password: String = "",
-    val error: String? = null
+    val password: String = ""
 )
